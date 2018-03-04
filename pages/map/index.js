bda1340f3248ced3dc6c5840b04c2714292bb31f
hzchenbo2014 <hzchenbo2014@corp.netease.com>
2018-03-04 22:41:13 +0800

    first commit

---
+++ pages/map/index.js
@@ -0,0 +1,20 @@
+Page({
+    data: {
+        latitude: 28.5928600000,
+        longitude: 119.2772100000,
+        markers: [{
+            latitude: 28.5928600000,
+            longitude: 119.2772100000,
+            name: '元立国际饭店'
+        }],
+        covers: [{
+            latitude: 28.5928600000,
+            longitude: 119.344520,
+            iconPath: '/image/location.png'
+        }, {
+            latitude: 28.5928600000,
+            longitude: 119.304520,
+            iconPath: '/image/location.png'
+        }]
+    }
+})
